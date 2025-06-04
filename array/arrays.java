b666486c6c6cdb3a0e415f9073f75b4d8d3e64f6
aayush patidar <ap6887980@gmail.com>
2025-06-04 11:01:23 +0530

    kadanes algorithm

--- array/arrays.java
+++ array/arrays.java
@@ -29,8 +29,24 @@
 public class arrays {
 
     public static void main(String args[]) {
-        int arr[] = { 1, 2, 3, 4 };
-        sumOfSubArray(arr);
+        int arr[] = { -1, 12, -1, -4 };
+        // sumOfSubArray(arr);
+        kadansSumOfArray(arr);
+    }
+
+    public static void kadansSumOfArray(int arr[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+
+            currSum = currSum + arr[i];
+            if (currSum < 0) {
+                currSum = 0;
+            }
+            maxSum = Math.max(maxSum, currSum);
+
+        }
+        System.out.println("Max is: " + maxSum);
     }
 
     public static void sumOfSubArray(int arr[]) {
