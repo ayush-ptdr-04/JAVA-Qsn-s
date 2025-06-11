f13c34e6a5ed20c39d4fdf61b9dcb2ece0d40054
aayush patidar <ap6887980@gmail.com>
2025-06-11 22:59:00 +0530

    leetCode qsn

--- array/arrays.java
+++ array/arrays.java
@@ -24,54 +24,84 @@
  * }
  */
 
-/* print max-subArrays-sum(Prfix array) */
+/*
+ * print max-subArrays-sum(Prfix array)
+ * 
+ * public class arrays {
+ * 
+ * public static void main(String args[]) {
+ * int arr[] = { -1, 12, -1, -4 };
+ * // sumOfSubArray(arr);
+ * kadansSumOfArray(arr);
+ * }
+ * 
+ * public static void kadansSumOfArray(int arr[]) {
+ * int currSum = 0;
+ * int maxSum = Integer.MIN_VALUE;
+ * for (int i = 0; i < arr.length; i++) {
+ * 
+ * currSum = currSum + arr[i];
+ * if (currSum < 0) {
+ * currSum = 0;
+ * }
+ * maxSum = Math.max(maxSum, currSum);
+ * 
+ * }
+ * System.out.println("Max is: " + maxSum);
+ * }
+ * 
+ * public static void sumOfSubArray(int arr[]) {
+ * int currSum = 0;
+ * int max = Integer.MIN_VALUE;
+ * int prefix[] = new int[arr.length];
+ * 
+ * prefix[0] = arr[0];
+ * 
+ * for (int i = 1; i < arr.length; i++) {
+ * prefix[i] = prefix[i - 1] + arr[i];
+ * }
+ * 
+ * for (int i = 0; i < arr.length; i++) {
+ * int start = i;
+ * for (int j = i; j < arr.length; j++) {
+ * int end = j;
+ * 
+ * currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
+ * 
+ * if (max < currSum) {
+ * max = currSum;
+ * }
+ * }
+ * }
+ * System.out.println("max is: " + max);
+ * }
+ * }
+ */
 
 public class arrays {
 
     public static void main(String args[]) {
-        int arr[] = { -1, 12, -1, -4 };
-        // sumOfSubArray(arr);
-        kadansSumOfArray(arr);
+        int height[] = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+        System.out.println(maxArea(height));
     }
 
-    public static void kadansSumOfArray(int arr[]) {
-        int currSum = 0;
-        int maxSum = Integer.MIN_VALUE;
-        for (int i = 0; i < arr.length; i++) {
+    public static int maxArea(int height[]) {
+        int n = height.length;
+        int mostWater = Integer.MIN_VALUE;
 
-            currSum = currSum + arr[i];
-            if (currSum < 0) {
-                currSum = 0;
-            }
-            maxSum = Math.max(maxSum, currSum);
-
-        }
-        System.out.println("Max is: " + maxSum);
-    }
-
-    public static void sumOfSubArray(int arr[]) {
-        int currSum = 0;
-        int max = Integer.MIN_VALUE;
-        int prefix[] = new int[arr.length];
-
-        prefix[0] = arr[0];
-
-        for (int i = 1; i < arr.length; i++) {
-            prefix[i] = prefix[i - 1] + arr[i];
-        }
-
-        for (int i = 0; i < arr.length; i++) {
-            int start = i;
-            for (int j = i; j < arr.length; j++) {
-                int end = j;
-
-                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
-
-                if (max < currSum) {
-                    max = currSum;
+        for (int i = 0; i < n; i++) {
+            int count = 1;
+            for (int j = i + 1; j < n; j++) {
+                int water = Math.min(height[i], height[j]);
+                if (water != 0) {
+                    int unitsWater = water * count;
+                    mostWater = Math.max(mostWater, unitsWater);
                 }
+                count++;
             }
         }
-        System.out.println("max is: " + max);
+
+        return mostWater;
+
     }
 }
