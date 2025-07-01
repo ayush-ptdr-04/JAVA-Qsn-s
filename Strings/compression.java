301b280afd8105d4b35376b0ffb281fc6ea368c6
aayush patidar <ap6887980@gmail.com>
2025-07-01 13:13:28 +0530

    complete String

---
+++ Strings/compression.java
@@ -0,0 +1,25 @@
+package Strings;
+
+public class compression {
+   public static void main(String args[]) {
+      String str = "aaabbccsss";
+      System.out.println(stringCompression(str));
+   }
+
+   public static String stringCompression(String str) {
+      StringBuilder newStr = new StringBuilder("");
+      for (int i = 0; i < str.length(); i++) {
+         Integer count = 1;
+         while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+            count++;
+            i++;
+         }
+         newStr.append(str.charAt(i));
+
+         if (count > 1) {
+            newStr.append(count.toString());
+         }
+      }
+      return newStr.toString();
+   }
+}
